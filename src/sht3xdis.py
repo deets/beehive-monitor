7077ef2224bdb443af43412e22b3859441f4408b
Diez B. Roggisch <deets@web.de>
2021-05-13 21:06:07 +0200

    Some readme update and actually reading humidity and temperature
    The resulting numbers are not really that convincing. But according
    to
    
    https://github.com/adafruit/Adafruit_SHT31
    
    the code looks good. Need further investigation.

--- src/sht3xdis.py
+++ src/sht3xdis.py
@@ -1,13 +1,24 @@
 import machine
+import ustruct
 
 
 class SHT3XDIS:
 
     STATUS = bytearray([0xF3, 0x2D])
+    MEASUREMENT = bytearray([0x24, 0x00])
+    MEASUREMENT_TIME = 20 # according to datasheet, 15. I add a bit extra.
+    RESET = bytearray([0x30, 0xA2])
+    CLEAR = bytearray([0x30, 0x41])
 
     def __init__(self, i2c, address=0x44):
         self._i2c = i2c
         self._address = address
+
+    def reset(self):
+        self._i2c.writeto(self._address, self.RESET)
+
+    def clear(self):
+        self._i2c.writeto(self._address, self.CLEAR)
 
     @property
     def status(self):
@@ -15,11 +26,28 @@ class SHT3XDIS:
         v = self._i2c.readfrom(self._address, 3)
         return v
 
+    @property
+    def values(self):
+        self._i2c.writeto(self._address, self.MEASUREMENT)
+        machine.sleep(self.MEASUREMENT_TIME)
+        v = self._i2c.readfrom(self._address, 6)
+        humidity, _, temp, _ = ustruct.unpack(">HBHB", v)
+        print(v, humidity, temp)
+        return (
+            float(humidity) / 65535.0 * 100,
+            -45.0 + 175.0 * float(temp) / 65535.0
+        )
+
 
 def test():
     scl = machine.Pin(12, machine.Pin.IN, machine.Pin.PULL_UP)
     sda = machine.Pin(14, machine.Pin.IN, machine.Pin.PULL_UP)
     i2c = machine.SoftI2C(scl=scl, sda=sda, freq=400000)
-    print(i2c.scan())
+
     sensor = SHT3XDIS(i2c)
+    sensor.reset()
+    machine.sleep(20)
+    sensor.clear()
+    machine.sleep(20)
     print(sensor.status)
+    print(sensor.values)
